78cbb031e80f0ed8c1e8c205b9b9338190b76f65
El-Congroo <103370616+El-Congroo@users.noreply.github.com>
2022-12-12 19:57:26 +0100

    Day11 Part 2 functioning, but way too slow :(

--- src/Day10.py
+++ src/Day10.py
@@ -1,10 +1,6 @@
 with open('data/input10.txt') as f:
     content = [line.strip().split(" ") for line in f.readlines()]
 
-
-
-
-
 sum = 0
 value = 1
 cycle = 0
@@ -23,10 +19,13 @@ for line in content:
         if (cycle) % 40 == 0:
             print('')
 
+
         # sum signal strength
         if (cycle % 40) - 20 == 0:
             sum += value * cycle
 
+
+        # addx number
         if i == 1:
             value += int(line[1])
 
---
+++ src/Day11.py
@@ -0,0 +1,93 @@
+monkeys = []
+
+
+class Monkey:
+
+    inspect = 0
+
+    def __init__(self, items, operation, divisibleBy, throwsTo):
+        self.items = items
+        self.operation = operation
+        self.divisibleBy = divisibleBy
+        self.throwsTo = throwsTo
+
+    def numberItems(self):
+        return len(self.items)
+
+    def operate(self):
+        old = self.items[0]
+        self.items[0] = eval(self.operation)
+        self.inspect += 1
+
+    def bored(self):
+        self.items[0] //= 3
+
+    def getActive(self):
+        return self.inspect
+
+    def test(self):
+        return self.items[0] % self.divisibleBy == 0
+
+    def throw(self):
+        i = 1
+        if self.test():
+            i = 0
+
+        return (self.throwsTo[i], self.items.pop(0))
+
+    def catch(self, item):
+        self.items.append(item)
+
+
+
+with open('data/sample.txt') as f:
+    content = [line.strip().replace(' ', '') for line in f.readlines()]
+
+
+# create monkeys
+for i in range(content.count('')+1):
+    line = i*7
+    items = list(map(int,content[line+1].split("items:")[1].split(',')))
+    operation = content[line+2].split("Operation:new=")[1]
+    divisibleBy = int(content[line+3].split("divisibleby")[1])
+    throwsTo = [int(content[line+4].split("monkey")[1]),
+                int(content[line+5].split("monkey")[1])]
+    
+    monkeys.append(Monkey(items, operation, divisibleBy, throwsTo))
+
+
+# run monkeys
+
+round = 10000
+
+for i in range(round):
+    for monkey in monkeys:
+        for j in range(monkey.numberItems()):
+            monkey.operate()
+            #monkey.bored()
+            catcher, item = monkey.throw()
+            monkeys[catcher].catch(item)
+
+    if i % 100 == 0:
+        print(i)
+
+
+
+activity = []
+for monkey in monkeys:
+    activity.append(monkey.getActive())
+    print(activity)
+
+
+    print(f"== After round {round} ==")
+for i in range(len(activity)):
+    print(f"Monkey {i} inspected items {activity[i]} times")
+
+prod = 1
+for _ in range(2):
+    highest = max(activity)
+    prod *= highest
+    activity.remove(highest)
+
+
+print(prod)
