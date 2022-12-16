666546c64e4ce82635df5052fc4fdf4b176322ab
El-Congroo <103370616+El-Congroo@users.noreply.github.com>
2022-12-16 10:31:42 +0100

    me right now

--- 2022/Day16.py
+++ 2022/Day16.py
@@ -1,4 +1,5 @@
 import re
+import copy
 
 #################
 ### read file ###
@@ -16,6 +17,62 @@ def readData():
 ##############
 ### part 1 ###
 ##############
+
+def getFollowUpTunnels(node, content):
+    for line in content:
+        if line[0][0] == node:
+            return line[1]
+
+def getPressure(node, content):
+    for line in content:
+        if line[0][0] == node:
+            return line[0][1]
+
+
+def setPressure(node, content, pressure):
+    for line in content:
+        if line[0][0] == node:
+            line[0][1] = pressure
+
+
+
+def getMaxPressure(node, content, minutesleft):
+    
+    if minutesleft <= 0:
+        return 0
+
+    if minutesleft == 1:
+        return getPressure(node, content)
+
+
+    maxRelease = 0
+
+    # open and walk (and open)
+    if minutesleft >= 3:
+        thisPressure = getPressure(node, content)
+        setPressure(node, content, 0)
+        for next in getFollowUpTunnels(node, content):
+            maxRelease = max(getMaxPressure(next, content, minutesleft-2), maxRelease)
+        maxRelease += thisPressure
+        setPressure(node, content, thisPressure) 
+
+    # just walk
+    if minutesleft >= 2:
+        for next in getFollowUpTunnels(node, content):
+            maxRelease = max(getMaxPressure(next, content, minutesleft-1), maxRelease)
+    
+    return maxRelease
+
+
+
+
+
+    
+
+def part1(content):
+    print(getMaxPressure(content[0][0], content, 30))
+
+
 
 for i in readData():
     print(i)
