c3a5f81cdd153940d3cffe89f9283eb1fbe7f087
El-Congroo <103370616+El-Congroo@users.noreply.github.com>
2022-12-19 23:59:34 +0100

    Day17 Part1 Working

--- 2022/Day16.py
+++ 2022/Day16.py
@@ -1,17 +1,18 @@
 import re
-import numpy as np
+
 
 #################
 ### read file ###
 #################
 
 def readData():
-    with open('2022/data/sample.txt') as f:
+    with open('data/input16.txt') as f:
         content = [[b for b in a.replace(",", "").strip().split("to valv")] for a in f.readlines()]
         for line in content:
             line[0] = [line[0].split(" ")[1], int(re.findall(r'-?[0-9]+', line[0])[0])]
             line[1] = line[1].split(" ")[1:]
         return content
+
 
 ##############
 ### part 1 ###
@@ -32,21 +33,22 @@ def getPressure(node, content):
         if line[0][0] == node:
             return line[0][1]
 
+
 def setListOfNodesRateBigger0(content):
     ret = []
     for line in content:
         if line[0][1] > 0:
             ret += [line[0][0]]
-    global nodesBigger0 
+    global nodesBigger0
     nodesBigger0 = ret
-    
+
 
 def getState(content):
     key = 0
     for node in nodesBigger0:
         key *= 2
         if getPressure(node, content) > 0:
-            key +=1
+            key += 1
     return key
 
 
@@ -63,7 +65,7 @@ def setDyn(content, minutesLeft, pos, i):
 def getDyn(content, minutesLeft, pos):
     if (minutesLeft, getState(content), pos) in dynamic:
         return dynamic[(minutesLeft, getState(content), pos)]
-    else: 
+    else:
         return -1
 
 
@@ -83,19 +85,18 @@ def getMaxPressure(node, content, minutesLeft):
 
     maxRelease = 0
 
-    """
     # open and walk
     if thisPressure > 0:
         setPressure(node, content, 0)
         for next in getNextNodes(node, content):
-            maxRelease = max(getMaxPressure(next, content, minutesLeft-2), maxRelease)
+            maxRelease = max(getMaxPressure(next, content, minutesLeft - 2), maxRelease)
 
-        maxRelease += thisPressure * (minutesLeft-1)
+        maxRelease += thisPressure * (minutesLeft - 1)
         setPressure(node, content, thisPressure)
 
     # just walk
     for next in getNextNodes(node, content):
-        maxRelease = max(getMaxPressure(next, content, minutesLeft-1), maxRelease)
+        maxRelease = max(getMaxPressure(next, content, minutesLeft - 1), maxRelease)
     """
 
     setPressure(node, content, 0)
@@ -103,11 +104,10 @@ def getMaxPressure(node, content, minutesLeft):
         maxRelease = max(getMaxPressure(next, content, minutesLeft -1 -np.sign(thisPressure)), maxRelease)
     maxRelease += thisPressure * (minutesLeft-1)
     setPressure(node, content, thisPressure)
-
+    """
 
     setDyn(content, minutesLeft, node, maxRelease)
     return getDyn(content, minutesLeft, node)
-
 
 
 def part1(content):
@@ -117,4 +117,17 @@ def part1(content):
     print("maxPressure:", val)
 
 
-part1(readData())
+def main():
+    import cProfile
+    import pstats
+
+    with cProfile.Profile() as pr:
+        print("Elements", "So the height is:", part1(readData()))
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.dump_stats(filename='needs_profiling.prof')
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2022/Day17.py
@@ -0,0 +1,165 @@
+rocks = [{0, 1, 2, 3},
+         {1 + 2j, 1j, 1 + 1j, 2 + 1j, 1},
+         {0, 1, 2, 2 + 1j, 2 + 2j},
+         {0, 1j, 2j, 3j},
+         {0, 1, 1j, 1 + 1j}]
+rocksDim = [4 + 1j, 3 + 3j, 3 + 3j, 1 + 4j, 2 + 2j]
+
+width = 7
+nrUnitsTat = 1_000_000_000_000
+nrUnits = nrUnitsTat
+sigSize = 40
+# del_every = 300
+
+
+def draw(title, tower, movedRock):
+    print(title, ":")
+    for y in range(10, -1, -1):
+        for x in range(width):
+            if x + y * 1j in tower:
+                print("#", end="")
+            elif x + y * 1j in movedRock:
+                print("@", end="")
+            else:
+                print(".", end="")
+        print("")
+    print("")
+
+
+def onGround(movedRock):
+    return movedRock.imag == 0
+
+
+def fall(rockPos):
+    return rockPos - 1j
+
+
+def setHeight(rockPos, i):
+    return rockPos.real + i*1j
+
+
+def moveBy(rockPos, rockId, i):
+    if -i > rockPos.real:
+        return rockPos.imag * 1j
+
+    if rockPos.real + rocksDim[rockId].real + i > width:
+        return (width-rocksDim[rockId].real) + rockPos.imag * 1j
+
+    return i + rockPos.real + rockPos.imag * 1j
+
+
+def move(content, rockPos, rockId, j):
+    i = content[j]
+
+    if rockPos.real + i < 0 or rockPos.real + rocksDim[rockId].real + i > width:
+        return rockPos
+
+    return rockPos + i
+
+
+def maxHeight(height, rockPos):
+    return max(height, rockPos.imag)
+
+
+def readData():
+    with open('data/sample.txt') as f:
+        return [[int(y.replace("<", "-1").replace(">", "1")) for y in x.strip()] for x in f.readlines()][0]
+
+
+def isOverlapping(rockPos, rockId, tower):
+    for i in getRockList(rockPos, rockId):
+        if i in tower:
+            return True
+        return False
+    # return not isNotOverlapping(rockPos, rockId, tower)
+
+
+def isNotOverlapping(rockPos, rockId, tower):
+    return not isOverlapping(rockPos, rockId, tower)
+    # return set(getRockSet(rockPos, rockId)).isdisjoint(tower)
+
+
+def getRockList(rockPos, rockId):
+    return [x + rockPos for x in rocks[rockId]]
+
+
+def getRockSet(rockPos, rockId):
+    a = set()
+    for i in rocks[rockId]:
+        a.add(i + rockPos)
+    return a
+
+
+def getSignature(tower, height):
+    return frozenset([height*1j - x for x in tower if height - x.imag <= sigSize])
+
+
+def partOne():
+    content = readData()
+    tower, beforeBlock, testSet = set(), set(), set()
+    height, added, i, ptr = 0, 0, 0, 0
+    seen = {}
+
+    while i < nrUnits:
+        rockId = i % len(rocks)
+        offsetStartPos = 0
+
+        for k in range(3):
+            offsetStartPos += content[ptr]
+            ptr = (ptr+1) % len(content)
+
+        rockPos = moveBy(height * 1j + 2, rockId, offsetStartPos)
+
+        while isNotOverlapping(rockPos, rockId, tower):
+            movedRockPos = move(content, rockPos, rockId, ptr)
+            ptr = (ptr+1) % len(content)
+
+            if isOverlapping(movedRockPos, rockId, tower):
+                movedRockPos = rockPos
+
+            if onGround(movedRockPos):
+                break
+
+            rockPos = fall(movedRockPos)
+
+        tower = tower.union(getRockSet(movedRockPos, rockId))
+        height = maxHeight(height, movedRockPos + rocksDim[rockId])
+
+        signature = getSignature(tower, height)
+        sr = (ptr, rockId, signature)
+
+        if sr in seen and i > 2022:
+            oldi, oldHeight = seen[sr]
+            dy = height-oldHeight
+            di = i-oldi
+            amt = (nrUnits - i) // di
+            added += amt*dy
+            i += amt*di
+            assert i <= nrUnits
+
+        seen[sr] = (i, height)
+
+#       Attempt to boost performance
+#       if i % del_every == 0:
+#           tower = tower - beforeBlock
+#           beforeBlock = tower
+
+        i += 1
+
+    return height + added
+
+
+def main():
+    import cProfile
+    import pstats
+
+    with cProfile.Profile() as pr:
+        print(partOne())
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.dump_stats(filename='needs_profiling.prof')
+
+
+if __name__ == "__main__":
+    main()
