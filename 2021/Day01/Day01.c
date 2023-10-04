f1666c3522772703f6ffdaf95a359668687a5d1c
El-Congroo <00.rouges.lapse@icloud.com>
2023-10-04 14:49:02 +0200

    added Day 01

--- 2021/Day01/Day01.c
+++ 2021/Day01/Day01.c
@@ -1,13 +1,35 @@
 #include "../input.h"
+#include <xlocale.h>
+#define WINDOWSZIE 3
+
+int getWindow(char **input, int index) {
+    int sum = 0;
+    for(int i=index; i<index+WINDOWSZIE; i++)
+        sum += atoi(input[i]);
+    return sum;
+}
 
 int main(int argc, char **argv) {
-
-    int len;
+    // ---------- //
+    // read input //
+    // ---------- //
+    int len, count = 0;
     char **input = readInput("data/input01.txt", &len);
 
-    for(int i=0; i<len; i++) {
-        printf("%s\n", input[i]);
-    }
-    
+    // -------- //
+    // part one //
+    // -------- //
+    for(int i=1; i<len; i++)
+        if (atoi(input[i-1]) < atoi(input[i])) 
+            count++;
+    printf("Part 1: %d\n", count);
 
+    // -------- //
+    // part two //
+    // -------- //
+    count = 0;
+    for(int i=1; i<len-WINDOWSZIE+1; i++)
+        if (getWindow(input, i - 1) < getWindow(input, i))
+            count++;
+    printf("Part 2: %d\n", count);
 }
