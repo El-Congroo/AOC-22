05eb108fc3ef805363cb135b8b05f40338a5d3ff
El-Congroo <00.rouges.lapse@icloud.com>
2023-12-03 21:10:12 +0100

    added 2023 day01

---
+++ 2023/Day01.py
@@ -0,0 +1,34 @@
+import re
+
+with open('2023/data/input01.txt') as f:
+    content = [line for line in f.readlines()]
+
+sum = 0
+for line in content:
+    numbers = [int(i) for i in re.findall(r'\d', line)]
+    sum += numbers[0] * 10 + numbers[-1]
+
+print("Part One:", sum)
+
+sum = 0 
+numberDict = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4, 
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+def parseToInt(n):
+    if n in numberDict:
+        return numberDict[n]
+    return(int(n))
+
+for line in content:
+    numbers = [match.group(1) for match in re.finditer(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', line)]
+    sum += parseToInt(numbers[0]) * 10 + parseToInt(numbers[-1])
+print("Part two:", sum)
