8a64c5e08debfa1e7310f4dbf5059b4dad6c1728
El-Congroo <00.rouges.lapse@icloud.com>
2023-12-04 12:54:58 +0100

    added Day 03 Part 2

--- 2023/Day03.py
+++ 2023/Day03.py
@@ -7,21 +7,33 @@ with open('2023/data/input03.txt') as f:
     content.append(border)
 
 def isAdjacent(i, start, end):
-    if content[i][start-1] != "." or content[i][end] != ".":
-        return True
-    for mov in range(-1,2,2):
+    for mov in range(-1,2):
         if re.search(r'[^.\d]', content[i+mov][start-1:end+1]):
             return True
     return False
-        
+
+def getPotGear(i, start, end):
+    pot = []
+    for mov in range(-1,2):
+        for match in re.finditer(r'[*]', content[i+mov][start-1:end+1]):
+            pot.append((start+match.start(), i+mov))
+    return pot
+
 
 sumOne = 0
+sumTwo = 0
 i = 1
+potGears = {}
 for line in content[1:-1]:
-    print(line)
     for match in re.finditer(r'\d+', line):
+        for position in getPotGear(i, match.start(), match.end()):
+            if position in potGears:
+                sumTwo += potGears[position] * int(match.group())
+            else:
+                potGears[position] = int(match.group())
         if isAdjacent(i, match.start(), match.end()):
             sumOne += int(match.group())
     i += 1
 
-print(sumOne)
+print("Part One:", sumOne)
+print("Part Two:", sumTwo)
