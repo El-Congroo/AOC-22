8a7df0599e71195325b5b1ba86aa05d7140bcf1c
El-Congroo <00.rouges.lapse@icloud.com>
2023-12-06 18:37:41 +0100

    optimized bbcnt

--- 2023/Day06.py
+++ 2023/Day06.py
@@ -10,11 +10,9 @@ def getDistance(msHeldDown, msWhole):
     return msHeldDown * (msWhole - msHeldDown)
 
 def bbcnt(msWhole, bestDistace): # bigger best count
-    for i in range(msWhole+1):
-        if getDistance(i, msWhole) > bestDistace:
-            return msWhole-i*2+1
+    x1 =  int(-((msWhole**2 - 4 * bestDistace)**(1/2) - msWhole) / 2) + 1
+    return msWhole-x1*2+1
     
-
 prod1 = 1
 for time, bestD in numbers:
     prod1 *= bbcnt(time, bestD) 
