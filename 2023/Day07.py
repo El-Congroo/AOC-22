c07ba0e66542a1390e19162e3ce11b4ab2685e19
El-Congroo <00.rouges.lapse@icloud.com>
2023-12-08 23:24:42 +0100

    removed one function

--- 2023/Day07.py
+++ 2023/Day07.py
@@ -1,35 +1,15 @@
 inputData = '2023/data/input07.txt'
 
-def getTypeRankOne(hand, labels):
-    cnt = []
-    for l in labels:
-        rank = hand.count(l)
-        if rank > 0:
-            cnt.append(rank)
-
-    match max(cnt):
-        case 5:
-            return 6
-        case 4:
-            return 5
-        case 3:
-            if min(cnt) == 2: return 4
-            return 3
-        case 2:
-            if cnt.count(2) == 2: return 2
-            return 1
-        case 1:
-            return 0
-        
-def getTypeRankTwo(hand, labels):
+def getTypeRank(hand, labels):
     cnt = [0]
-    for l in labels[1:]:
+    for l in labels[1:] if labels[0] == "J" else labels:
         rank = hand.count(l)
         if rank > 0:
             cnt.append(rank)
     cnt.sort()
-    cnt[-1] += hand.count("J")
+    if labels[0] == "J": cnt[-1] += hand.count("J")
     if(cnt[0] == 0): cnt = cnt[1:]
+
     match max(cnt):
         case 5:
             return 6
@@ -44,7 +24,7 @@ def getTypeRankTwo(hand, labels):
         case 1:
             return 0
 
-def getRankValue(hand, labels, getTypeRank):
+def getRankValue(hand, labels):
     priority = len(hand)
     lableCnt = len(labels)
     rank = getTypeRank(hand, labels) * (lableCnt ** priority)
@@ -79,7 +59,7 @@ def getSum(content):
 
 labelsOne = list("AKQJT98765432")[::-1]
 with open(inputData) as f:
-    content = [(getRankValue(y, labelsOne, getTypeRankOne), y, int(z)) for y, z in [x.split(" ") for x in f.readlines()]]
+    content = [(getRankValue(hand, labelsOne), hand, int(val)) for hand, val in [x.split(" ") for x in f.readlines()]]
 quickSort(content, 0, len(content)-1)
 
 print("Part one", getSum(content))
@@ -87,6 +67,6 @@ print("Part one", getSum(content))
 
 labelsTwo = list("AKQT98765432J")[::-1]
 with open(inputData) as f:
-    content = [(getRankValue(y, labelsTwo, getTypeRankTwo), y, int(z)) for y, z in [x.split(" ") for x in f.readlines()]]
+    content = [(getRankValue(y, labelsTwo), y, int(z)) for y, z in [x.split(" ") for x in f.readlines()]]
 quickSort(content, 0, len(content)-1)
 print("Part two", getSum(content))
