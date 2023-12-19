e2c70cb37313279c0eeff2733b785cf9d8fa6b5e
El-Congroo <00.rouges.lapse@icloud.com>
2023-12-19 18:25:52 +0100

    Part one done

--- 2023/Day11.py
+++ 2023/Day11.py
@@ -1,12 +1,30 @@
 with open('2023/data/input11.txt') as f:
     content = [line.strip() for line in f.readlines()]
 
+def doubleEmpty(galaxy):
+    i = 0
+    while i < len(galaxy):
+        if '#' not in galaxy[i]:
+            galaxy.insert(i, "."*len(galaxy[i]))
+            i += 1
+        i += 1
 
+doubleEmpty(content)
+content = [''.join(row) for row in zip(*reversed(content))]
+doubleEmpty(content)
 
-x, y = 0, 0
-for i in range(len(content)):
-    if '#' not in content[i]: y +=1
-    if '#' not in ''.strip([x[i] for x in content]): x += 1
+allGalaxies = []
 
-    x += 1
-    y += 1
+for y in range(len(content)):
+    for x in range(len(content[0])):
+        if content[y][x] == '#':
+            allGalaxies.append((y, x))
+
+galCnt = len(allGalaxies)
+
+distsum = 0 
+for i in range(galCnt):
+    for j in range(i+1, galCnt):
+        distsum += sum(map(lambda a, b: abs(a-b), allGalaxies[i], allGalaxies[j]))
+
+print("Part one:", distsum)
