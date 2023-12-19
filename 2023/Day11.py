dd5fa7213d082238ca6296dd49f60ed442dc8de1
El-Congroo <00.rouges.lapse@icloud.com>
2023-12-19 20:45:58 +0100

    added Day 11 Part two and updated Part one

--- 2023/Day11.py
+++ 2023/Day11.py
@@ -1,30 +1,36 @@
 with open('2023/data/input11.txt') as f:
     content = [line.strip() for line in f.readlines()]
 
-def doubleEmpty(galaxy):
-    i = 0
-    while i < len(galaxy):
-        if '#' not in galaxy[i]:
-            galaxy.insert(i, "."*len(galaxy[i]))
-            i += 1
-        i += 1
+def getDistSum(allGalaxies):
+    galCnt, distsum = len(allGalaxies), 0
+    for i in range(galCnt):
+        for j in range(i+1, galCnt):
+            distsum += sum(map(lambda a, b: abs(a-b), allGalaxies[i], allGalaxies[j]))
+    return distsum
 
-doubleEmpty(content)
-content = [''.join(row) for row in zip(*reversed(content))]
-doubleEmpty(content)
+def getAllGalaxPos(dist):
+    allGalaxies, allGala, disty, distx = {}, [], 0, 0
+    l = range(len(content))
 
-allGalaxies = []
+    for i in l:
+        if '#' not in content[i]: disty += 1
+        else: 
+            for j in l: 
+                if content[i][j] == '#':
+                    if (i, j) not in allGalaxies: allGalaxies[(i, j)] = [0, 0]
+                    allGalaxies[(i, j)][0] = disty
 
-for y in range(len(content)):
-    for x in range(len(content[0])):
-        if content[y][x] == '#':
-            allGalaxies.append((y, x))
+        line = [x[i] for x in content]
+        if '#' not in line: distx += 1
+        else:
+            for j in l:
+                if line[j] == '#':
+                    if (j, i) not in allGalaxies: allGalaxies[(j, i)] = [0, 0]
+                    allGalaxies[(j, i)][1] = distx
 
-galCnt = len(allGalaxies)
+    for k in allGalaxies.keys():
+        allGala.append(tuple(map(lambda a, b, d: a + (b * d), k, allGalaxies.get(k), [dist for _ in range(2)])))
+    return allGala
 
-distsum = 0 
-for i in range(galCnt):
-    for j in range(i+1, galCnt):
-        distsum += sum(map(lambda a, b: abs(a-b), allGalaxies[i], allGalaxies[j]))
-
-print("Part one:", distsum)
+print("Part one:", getDistSum(getAllGalaxPos(1)))
+print("Part two:", getDistSum(getAllGalaxPos(1000000-1)))
