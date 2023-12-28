0723b3a693b6e52a7e571f10179c4f5040205352
El-Congroo <00.rouges.lapse@icloud.com>
2023-12-28 02:04:04 +0100

    added store result for first aproach

--- 2023/Day12.py
+++ 2023/Day12.py
@@ -3,24 +3,21 @@ with open('2023/data/input12.txt') as f:
 
 cache = {}
 
-def getArrCnt(condition, numbers):
-    if len(numbers) == 0: return 1 if "#" not in condition else 0
-    if len(condition) == 0: return 0
-
-    if key := (condition, numbers) in cache: return cache[key]
+def getArrCnt(key):
+    condition, numbers = key
+    if numbers == (): return 1 if "#" not in condition else 0
+    if condition == "": return 0
 
     ret, n = 0, numbers[0]
-
-    if condition[0] in ".?":
-        ret += getArrCnt(condition[1:], numbers)
-        
-    if condition[0] in "#?":
-        if n <= len(condition) and "." not in condition[:n] and (n == len(condition) or condition[n] != "#"):
-            ret += getArrCnt(condition[n+1:], numbers[1:])
-        
-    cache[key] = ret
+    for i in range(len(condition) - n + 1):
+        if (i != 0 and condition[i-1] == "#"): break
+        if "." not in condition[i:i+n] and (i+n == len(condition) or condition[i+n] != "#"):
+            key = (condition[i+n+1:], numbers[1:])
+            if key not in cache: 
+                cache[key] = getArrCnt(key)
+            ret += cache[key]
     return ret
 
 
-print("Part one:", sum(getArrCnt(condition, tuple(map(int, numbers.split(",")))) for condition, numbers in content))
-print("Part two:", sum(getArrCnt("?".join([condition]*5), tuple(map(int, numbers.split(",")))*5) for condition, numbers in content))
+print("Part one:", sum(getArrCnt((condition, tuple(map(int, numbers.split(","))))) for condition, numbers in content))
+print("Part two:", sum(getArrCnt(("?".join([condition]*5), tuple(map(int, numbers.split(",")))*5)) for condition, numbers in content))
