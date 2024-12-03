6ae563aa47c73883061fbae920061f64d0812d7c
Lasse Krähner <lasse.kraehner@fau.de>
2024-12-03 02:57:30 +0100

    added Day02 - 2024

--- 2024/Day02.py
+++ 2024/Day02.py
@@ -1,13 +1,13 @@
 with open('2024/data/input02.txt') as f:
-    c = [[int(x) for x in line.split(" ")] for line in f]
+    c = [[int(i) for i in l.split(" ")] for l in f]
 
-getDiff = lambda l: [a - b for a,b in zip(l[:-1], l[1:])]
-rmIdx = lambda l, d, m: (lambda i: l[:i] + l[i+1:])(d.index(m(d)))
-chkDiff = lambda d: (0 < min(d) <= max(d) <= 3) or (-3 <= min(d) <= max(d) < 0)
+dif = lambda l: [a-b for a, b in zip(l[:-1], l[1:])]
+chk = lambda d: (0<min(d)<=max(d)<=3) or (-3<=min(d)<=max(d)<0)
+rmv = lambda l, d, m: (lambda i: l[:i] + l[i+1:])(d.index(m(d)))
 
 pt1 = pt2 = 0
 for l in c:
-    if chkDiff(d := getDiff(l)): pt1 += 1; pt2 += 1
-    elif any(chkDiff(getDiff(i)) for i in [rmIdx(l, d, max), rmIdx(l, d, min), l[:-1]]): pt2 += 1
+    if chk(d := dif(l)): pt1 += 1; pt2 += 1
+    elif any(chk(dif(i)) for i in [rmv(l, d, max), rmv(l, d, min), l[:-1]]): pt2 += 1
 
-[print(sol) for sol in [f"Part 1: {pt1}", f"Part 2: {pt2}"]]
+[print(s) for s in [f"Part 1: {pt1}", f"Part 2: {pt2}"]]
