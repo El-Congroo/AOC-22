c22db50db9246e4052f4e38237c07a7afeb8b1d8
Lasse Krähner <lasse.kraehner@fau.de>
2024-12-03 02:49:11 +0100

    added Day01 - 2024

--- 2024/Day01.py
+++ 2024/Day01.py
@@ -2,4 +2,4 @@ with open('2024/data/input01.txt') as f:
     [left, right] = [sorted(l) for l in zip(*[list(map(int, line.split("   "))) for line in f])]
 
 print("Part 1:", sum([abs(x-y) for x,y in zip(left, right)]))
-print("Part 2:", sum(l * right.count(l)for l in set(left)))
+print("Part 2:", sum(l * right.count(l) for l in set(left)))
