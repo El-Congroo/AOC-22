ba83b4bbae6eaaa17b8746fbb66fc9d7a5ea7729
Lasse Krähner <lasse.kraehner@fau.de>
2024-12-05 13:33:14 +0100

    trying Day05 - 2024

---
+++ 2024/Day05.py
@@ -0,0 +1,38 @@
+with open('2024/data/input05.txt') as f:
+    [rules, orders] = [x.split("\n") for x in f.read().split("\n\n")]
+    r = {}
+    for rule in rules:
+        [o, b] = map(int, rule.split("|"))
+        r.setdefault(o, set()).add(b)
+
+def isCorrectOrder(order : list) -> bool:
+    for o in order:
+        if len(r.setdefault(o, set()) & set(order[:order.index(o)])) > 0:
+            return False
+    return True
+
+def sort(order: list) -> list:
+    for i in range(len(order)-1, 0, -1):
+        for j in r.setdefault(order[i], []):
+            if j in order[:i]:
+                order.remove(j)
+                order.insert(i, j)
+    return order
+
+
+pt1, pt2 = 0, 0
+for order in orders[:-1]:
+    order = list(map(int, order.split(",")))
+    mid = len(order) // 2
+    
+    if isCorrectOrder(order):
+        pt1 += order[mid]
+    else: 
+        pt2 += sort(order)[mid]
+
+
+print("Part 1:", pt1)
+print("Part 2:", pt2)
+
+
+# 4002 < 
