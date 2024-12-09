38c07417ab531ff20e7f3e4d256067dcdd329a47
Lasse Krähner <lasse.kraehner@fau.de>
2024-12-09 17:00:29 +0100

    commented Day09 - 2024

--- 2024/Day09.py
+++ 2024/Day09.py
@@ -2,8 +2,6 @@
 with open('2024/data/input09.txt') as f:
     content = [int(y) for x in f for y in x.strip()]
 
-assert len(content) % 2 == 1
-
 cur_front_id = 0
 cur_back_id = (len(content) - 1) // 2 + 1
 fill_in = content[::-2]
@@ -30,7 +28,8 @@ for x in content:
         if i == (all_cnt - free_cnt): finished = True; break
     if finished: break
     if not (free := not free): cur_front_id += 1
-    
+
+# 6330095022244
 print("Part 1:", sum1)
 
 def draw(c):
@@ -40,49 +39,59 @@ def draw(c):
 
 enumerated_content = [(i//2, x) if i % 2 == 0 else (None, x) for i, x in enumerate(content) if x > 0]
 
-print(enumerated_content)
-
 reverse_enum_blocks = [x for x in enumerated_content[::-1] if x[0] != None]
 
-def mergeCheck(index):
-    mergeStorage = 0
+def merge(index):
+    mergeStorage = space = 0
     while enumerated_content[index][0] == None:
-        mergeStorage += enumerated_content[index][1]
+        mergeStorage += (space := enumerated_content[index][1])
         del enumerated_content[index]
         if index > len(enumerated_content) - 1: break
     if mergeStorage > 0:
         enumerated_content.insert(index, (None, mergeStorage))
+    return (mergeStorage > space)
 
-for b in reverse_enum_blocks:
-    print(b, end=": ")
+# go through all possibly movable blocks
+for b in reverse_enum_blocks: 
+    # print(b, end=": ")
     # draw(enumerated_content)
+    
+    # go over (free) blocks
     for i in range(len(enumerated_content)):
-        if enumerated_content[i] == b: break
-        if None == enumerated_content[i][0] and ((space := enumerated_content[i][1]) >= b[1]):
-            j = enumerated_content.index(b)
+        
+        # stop if there is no free space before cur element
+        if enumerated_content[i] == b: break 
+        
+        # check if free and enough space
+        if (None == enumerated_content[i][0]) and ((free_space := enumerated_content[i][1]) >= b[1]):
+            
+            # delete element being moved
+            j = enumerated_content.index(b) 
             del enumerated_content[j]
-            enumerated_content.insert(j, (None, b[1]))
+            enumerated_content.insert(j, (None, occupied_space := b[1]))
             
-            mergeCheck(j-1)
+            # merges free block
+            merge(j-1)
             
+            # delete empty space
             del enumerated_content[i]
-            if (space := space - b[1]) > 0:
-                enumerated_content.insert(i, (None, space))
+            
+            # insert free block and merge
+            if (free_space := free_space - occupied_space) > 0:
+                enumerated_content.insert(i, (None, free_space))
+                if(merge(i - 1)): print("merged!")
+            
+            # insert blocked block
             enumerated_content.insert(i, b)
             break
 
-sum2 = 0
-print(enumerated_content)
-i = 0
+sum2, i = 0, 0
 for x, j in enumerated_content:
     if x == None:
         i += j
         continue
     for _ in range(j):
-        sum2 += x * (i)
+        sum2 += x * i
         i += 1
-        
-print("Part 2:", sum2)
 
-
-# != 6359444896937
+print("Part 2:", sum2) # != 6359444896937
